143797ceb1335c2f990dca88d629ba3ce8f2e427
Ali Hassan <62383138+alihassant@users.noreply.github.com>
2024-09-17 04:19:58 +0500

    create user route post trigger

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -11,3 +11,25 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({ error: `Error retrieving users: ${err.message}` });
   }
 };
+
+export const postUser = async (req: Request, res: Response) => {
+  try {
+    const {
+      username,
+      cognitoId,
+      profilePictureUrl = "i1.jpg",
+      teamId = 1,
+    } = req.body;
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        cognitoId,
+        profilePictureUrl,
+        teamId,
+      },
+    });
+    res.json({ message: "User Created Successfully", newUser });
+  } catch (err: any) {
+    res.status(500).json({ error: `Error retrieving users: ${err.message}` });
+  }
+};
